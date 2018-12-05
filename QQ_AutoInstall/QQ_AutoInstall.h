fb8dd396b10c5118da4148990cf0de5a51d4b88b
wuqiong <29012101@qq.com>
2018-12-05 09:52:10 +0800

    Initial Commit

---
+++ QQ_AutoInstall/QQ_AutoInstall.h
@@ -0,0 +1,19 @@
+//
+//  QQ_AutoInstall.h
+//  QQ_AutoInstall
+//
+//  Created by Mac on 2018/12/5.
+//  Copyright © 2018 Mac. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for QQ_AutoInstall.
+FOUNDATION_EXPORT double QQ_AutoInstallVersionNumber;
+
+//! Project version string for QQ_AutoInstall.
+FOUNDATION_EXPORT const unsigned char QQ_AutoInstallVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QQ_AutoInstall/PublicHeader.h>
+
+
